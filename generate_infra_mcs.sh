68d8a8fa3e2f4ab6cb63c692a86a0396c5f6bdb8
gpillon <gpillon@redhat.com>
2023-10-10 16:57:19 +0200

    translated

--- generate_infra_mcs.sh
+++ generate_infra_mcs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Assicurati di avere yq e oc installati
+# Ensure you have yq and oc installed
 
 process_file() {
     local file="$1"
@@ -8,32 +8,32 @@ process_file() {
     local output_dir="output"
     local output_file="$output_dir/${filename%.yaml}-infra.yaml"
 
-    # Crea la directory di output se non esiste
+    # Create the output directory if it doesn't exist
     mkdir -p "$output_dir"
 
-    # Verifica se il file di output esiste già e chiedi conferma per sovrascriverlo
+    # Check if the output file already exists and ask for confirmation to overwrite it
     if [ -f "$output_file" ]; then
-        read -p "Il file $output_file esiste già. Vuoi sovrascriverlo? (y/n) " choice
+        read -p "The file $output_file already exists. Do you want to overwrite it? (y/n) " choice
         case "$choice" in
-            y|Y) echo "Sovrascrittura del file $output_file...";;
-            *) echo "Saltando il file $file..."; return;;
+            y|Y) echo "Overwriting the file $output_file...";;
+            *) echo "Skipping the file $file..."; return;;
         esac
     fi
 
-    # Copia il file originale al nuovo file con suffisso -infra
+    # Copy the original file to the new file with -infra suffix
     cp "$file" "$output_file"
 
-    # Estrai le informazioni necessarie dal file YAML
+    # Extract necessary information from the YAML file
     infrastructure_id=$(yq eval '.metadata.labels."machine.openshift.io/cluster-api-cluster"' "$output_file")
     zone=$(echo $(yq eval '.metadata.name' "$output_file") | awk -F '-worker-' '{print $2}')
     
-    # Cambia il nome
+    # Change the name
     yq eval -i '.metadata.name = "'"$infrastructure_id"'-infra-'"$zone"'"' "$output_file"
     
-    # Aggiungi il taint
+    # Add the taint
     yq eval -i '.spec.template.spec.taints += [{"key": "node-role.kubernetes.io/infra", "effect": "NoSchedule"}]' "$output_file"
    
-    # Modifica le labels
+    # Modify the labels
     yq eval -i '.spec.template.metadata.labels."machine.openshift.io/cluster-api-machine-role" = "infra"' "$output_file"
     yq eval -i '.spec.template.metadata.labels."machine.openshift.io/cluster-api-machine-type" = "infra"' "$output_file"
     yq eval -i '.spec.template.metadata.labels."machine.openshift.io/cluster-api-machineset" = "'"$infrastructure_id"'-infra-'"$zone"'"' "$output_file"
@@ -41,28 +41,29 @@ process_file() {
     yq eval -i '.spec.selector.matchLabels."machine.openshift.io/cluster-api-machineset" = "'"$infrastructure_id"'-infra-'"$zone"'"' "$output_file"
     yq eval -i '.spec.template.spec.metadata.labels."node-role.kubernetes.io/infra" = ""' "$output_file"
 
-    # Rimuovi campi indesiderati e il campo "status"
+    # Remove unwanted fields and the "status" field
     yq eval -i 'del(.metadata.creationTimestamp, .metadata.resourceVersion, .metadata.selfLink, .metadata.uid, .status)' "$output_file"
 
-    echo "File $output_file modificato con successo."
+    echo "File $output_file successfully modified."
 }
 
 if [ -z "$1" ]; then
-    # Nessuna directory fornita, ottieni MachineSets dal cluster online
+    # No directory provided, get MachineSets from the online cluster
     oc get machinesets -l machine.openshift.io/cluster-api-machine-role=worker -o yaml > online_machinesets.yaml
-    # Dividi l'YAML combinato in file separati per ogni MachineSet
+    # Split the combined YAML into separate files for each MachineSet
     csplit -f machine_set_ online_machinesets.yaml '/^---$/' '{*}'
-    # Processa ogni file MachineSet separato
+    # Process each separate MachineSet file
     for file in machine_set_*; do
         process_file "$file"
         rm "$file"
     done
 else
     DIRECTORY="$1"
-    # Itera su tutti i file YAML nella directory fornita
+    # Iterate over all YAML files in the provided directory
     for file in $DIRECTORY/*.yaml; do
         process_file "$file"
     done
 fi
 
-echo "Operazione completata."
+echo "Operation completed."
+
